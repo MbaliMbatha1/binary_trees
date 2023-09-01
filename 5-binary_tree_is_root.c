63e59a59bba572dce80cd6ff3acbfff9a830150f
MbaliMbatha1 <124329266+MbaliMbatha1@users.noreply.github.com>
2023-09-01 21:37:06 +0200

    Task Done

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * 
+ * binary_tree_is_root - sdf agsdf gsdfg sdf
+ * @node: df gsdfh fgyh fgh fdg
+ *
+ * Return: 1g hdfgdyfghj dfgg
+ *         0fg hdfgjd hgj fdhgjd
+ *         0 hjdfhgjdf jd df
+ *
+ *
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+  
+	return ((!node || node->parent) ? 0 : 1);
+  
+}
