8f3ad77bb43f04085ba25a1f55e4ce55d809cf5b
MbaliMbatha1 <124329266+MbaliMbatha1@users.noreply.github.com>
2023-09-01 21:43:03 +0200

    Task Done

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - dfg sdgfh sdrfsdfg fdg
+ * @tree: dfg sdfg dfgsdf dfsgs fdg
+ * @func: gsdfg sdfgsdf hsg sdfgdsf gsdfg
+ *
+ *
+ *
+ */
+
+
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
