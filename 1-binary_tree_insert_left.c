3639325d1bc357c5a228ec899f3f4bffbdc8651b
MbaliMbatha1 <124329266+MbaliMbatha1@users.noreply.github.com>
2023-09-01 21:31:34 +0200

    Task Done

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,7 @@
  */
 
 
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
@@ -31,4 +32,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new->left)
 		new->left->parent = new;
 	return (new);
+	
 }
