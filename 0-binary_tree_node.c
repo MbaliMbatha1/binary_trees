ac75b1ced9a417a1812bcfafb8113502b2d0668e
MbaliMbatha1 <124329266+MbaliMbatha1@users.noreply.github.com>
2023-09-01 20:59:47 +0200

    Task Done

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,15 +1,19 @@
 #include "binary_trees.h"
 
 /**
+*
 * @parent: ptr
 * @value : new
 * Return: return
 *
 */
 
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+
 	binary_tree_t *new;
+
 
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
