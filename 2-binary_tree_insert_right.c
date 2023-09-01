c5bebfceda6d623e07325ee4bd871cf6bf47d1b7
MbaliMbatha1 <124329266+MbaliMbatha1@users.noreply.github.com>
2023-09-01 21:08:12 +0200

    Task Done

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_insert_right - fvfd dkjfv kdf b bdfk 
+ * @parent: lsf gsldfh glsfd lsdfj glsdfj g
+ * @value:  sdfjgklsjdhf glskdfjhgls fdj g
+ *
+ * Return: skdjf gsdf gsb dfbgs dfb
+ *         NUL sdhlfajsd  
+ *         NUasdfkaljh dslafkjhd s
+ */
+
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = parent->right;
+	parent->right = new;
+	if (new->right)
+		new->right->parent = new;
+	return (new);
+}
