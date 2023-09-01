3cffb6f02c726e85a662efb32d025ce47bef7279
Ranger03 <89092453+Ranger03@users.noreply.github.com>
2023-09-01 21:59:23 +0200

    Task Done

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,54 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - sdaf asdf asdfas dfa sdfasdfasd f
+ * @tree: sd asdf asdfasdfa sdfas dasdf asdfasdf sadf asd
+ *
+ * Return: s dfasdf asdf asdf as
+ *         0 a sdfasdf sfd sad fsd
+ *
+ *
+ *
+ *
+ */
+
+
+
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int height_l, height_r;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
+	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
+
+	return (height_l - height_r);
+}
+
+
+/**
+ * binary_tree_height -  sadfas dfsf asd fasdf asdfas  dsdf
+ * @tree: as dfasdf asdf asdf asdf asfd sfd
+ *
+ * Return: sd fasdf sadf asdf as
+ *         0 isd fa sdf asdf asdfasdf
+ *
+ *
+ */
+
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
