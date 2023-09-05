1e7f3a85d42375411820dd1037b75a8574e83f05
Ranger03 <89092453+Ranger03@users.noreply.github.com>
2023-09-05 17:04:44 +0200

    Task Done

---
+++ 123-avl_remove.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * avl_remove - asd jhfk ajhs dkfj has lkd fjha ssadf
+ * @root: a aksj dhfjasdlhfkasjdhf aklsjhd fklasdjhf askdfasd
+ * @value: ak djshkfljhas dlfjhasdklfaslkdjf haslkdjhf askdjhf alks
+ *
+ *
+ *
+ *
+ * Return: a asdfh ljkas dhfask jdfha skdjhf klasjdhfkajsd fkasd fasd
+ *         NULL on ask dfhgkasjhd
+ *
+ *
+ *
+ *
+ */
+avl_t *avl_remove(avl_t *root, int value)
+{
+
+}
